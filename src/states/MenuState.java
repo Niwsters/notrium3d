40f830d740051cf38c5b7c69325454526673291d
Enet4Mikeenet <Enet4Mikeenet@be5ce5f5-ed06-429f-7e02-e4aca6ba86d8>
2011-05-05 23:29:33 +0000

    Created MenuState.

---
+++ src/states/MenuState.java
@@ -0,0 +1,29 @@
+package states;
+
+import com.jme3.app.Application;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.renderer.RenderManager;
+
+import com.jme3.input.InputManager;
+import com.jme3.input.KeyInput;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
+
+public class MenuState extends AbstractAppState {
+	
+	public MenuState()
+	{
+	}
+	
+	public void update(float tpf)
+	{
+		
+	}
+
+	
+}
