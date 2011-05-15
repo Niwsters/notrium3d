bac8ff2f6228c44a8c545c456f847741875bc145
sebastian.vasser@gmail.com <sebastian.vasser@gmail.com@be5ce5f5-ed06-429f-7e02-e4aca6ba86d8>
2011-05-15 13:47:22 +0000

    Made a START button that blinks when you hover it! Next goal will be to make it do something.

---
+++ trunk/src/states/GameState.java
@@ -0,0 +1,12 @@
+package states;
+
+import com.jme3.app.state.AbstractAppState;
+
+public class GameState extends AbstractAppState {
+	
+	public GameState() {
+	}
+	
+	public void update(float tpf) {
+	}
+}
