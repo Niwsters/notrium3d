b7957d556e3e8074faa4c04635b435e3098e5ca7
sebastian.vasser@gmail.com <sebastian.vasser@gmail.com@be5ce5f5-ed06-429f-7e02-e4aca6ba86d8>
2011-05-15 13:47:22 +0000

    Made a START button that blinks when you hover it! Next goal will be to make it do something.

--- src/states/MenuState.java
+++ src/states/MenuState.java
@@ -5,21 +5,50 @@ import main.Notrium3D;
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.font.BitmapFont;
 import com.jme3.font.BitmapText;
+import com.jme3.input.MouseInput;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector2f;
 
 public class MenuState extends AbstractAppState{
 	
 	BitmapFont hudFont;
 	BitmapText hudText;
+	Notrium3D game;
 	
-	public MenuState(Notrium3D app) {
-		hudFont = app.getAssetManager().loadFont("Interface/Fonts/Default.fnt");
+	public MenuState(Notrium3D game) {
+		this.game = game;
 		
-		hudText = new BitmapText(hudFont, false);
-		hudText.setText("START");
-		hudText.setLocalTranslation(300, hudText.getLineHeight(), 0);
-		app.getGUINode().attachChild(hudText);
+		buildMenu();
 	}
 	
 	public void update(float tpf) {
+		
+		if(hudText.getLocalTranslation().getY()
+				>= game.getInputManager().getCursorPosition().y
+				&& hudText.getLocalTranslation().getY()
+				<= game.getInputManager().getCursorPosition().y + hudText.getLineHeight()
+				&& hudText.getLocalTranslation().getX() + hudText.getLineWidth()
+				>= game.getInputManager().getCursorPosition().x
+				&& hudText.getLocalTranslation().getX()
+				<= game.getInputManager().getCursorPosition().x) {
+			if(hudText.getColor() != ColorRGBA.Red) {
+				hudText.setColor(ColorRGBA.Red);
+			}
+		} else {
+			if(hudText.getColor() != ColorRGBA.White) {
+				hudText.setColor(ColorRGBA.White);
+			}
+		}
+	}
+	
+	private void buildMenu() {
+		hudFont = game.getAssetManager().loadFont("Interface/Fonts/Default.fnt");
+		
+		hudText = new BitmapText(hudFont, false);
+		hudText.setText("START");
+		hudText.setLocalTranslation(300,
+				hudText.getLineHeight()+(240-hudText.getLineHeight()/2),
+				0);
+		game.getGUINode().attachChild(hudText);
 	}
 }
